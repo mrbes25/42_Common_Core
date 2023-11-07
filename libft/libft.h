810682ef02157e34d62178b11f176085f99e01ad
mrbes25 <bastian123schmid@gmail.com>
2023-11-07 14:20:00 +0100

    added some bonuses

--- libft/libft.h
+++ libft/libft.h
@@ -60,11 +60,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));*/
 
-typedef	struct	s_list
+typedef struct s_list
 {
 	void			*content;
-	struct	s_list	*next;
+	struct s_list	*next;
 }					t_list;
-
 
 #endif
