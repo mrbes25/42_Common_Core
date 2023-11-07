1fd5deb7ba806bb61acd857d6c10c501e4423aa4
mrbes25 <bastian123schmid@gmail.com>
2023-11-07 13:23:34 +0100

    added first three bonus functions

--- libft/libft.h
+++ libft/libft.h
@@ -50,5 +50,21 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+/*t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));*/
+
+typedef	struct	s_list
+{
+	void			*content;
+	struct	s_list	*next;
+}					t_list;
+
 
 #endif
