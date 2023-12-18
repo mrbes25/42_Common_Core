900821f875b19e13b16f569d2487a9a96947dbfb
mrbes25 <bastian123schmid@gmail.com>
2023-12-18 17:38:58 +0100

    gnueg für hüt :)

--- get_next_line_v2/get_next_line.c
+++ get_next_line_v2/get_next_line.c
@@ -18,7 +18,7 @@ static char	*ft_read_join(int fd, char *str) // make shure it can handle it if t
 	int		bytes;
 	//printf("test_6\n");
 	bytes = BUFFER_SIZE; // enters while loop when it gets called again, this could cause the seg fault
-	while (bytes == BUFFER_SIZE)
+	while (bytes == BUFFER_SIZE || ft_strchr(buffer, '\n') != NULL)
 	{
 		if (!str)
 			str = malloc((2) * sizeof(char));
@@ -34,6 +34,8 @@ static char	*ft_read_join(int fd, char *str) // make shure it can handle it if t
 		str = ft_strjoin(str, buffer);
 		if (!str)
 			break ;
+		if (ft_strchr(buffer, '\n'))
+			break;
 	
 	}
 	// printf("test_1: %s\n", str);
@@ -49,20 +51,20 @@ char	*get_next_line(int fd)
 
 	str = ft_read_join(fd, str); //makes str to one big string, mallocs str if null
 	//printf("test_1: %s\n", str); 
-	temp_1 = ft_substr(str, 0, ft_count(str)); // fills temp_1 with line
 	if (!str)
+		return (NULL);
+	temp_1 = ft_substr(str, 0, ft_count(str)); // fills temp_1 with line
+	if (!temp_1)
 		return (NULL);
 	//printf("test_2: %s\n", temp_1);
 	temp_2 = ft_strchr(str, '\n');
 	if (temp_2 != NULL)
 		temp_2 = temp_2 + 1;
 	else
-		temp_2 = NULL; // sets temp_2 to pointer to new beginning of remaining str
+		temp_2 = str + ft_count(str); // sets temp_2 to pointer to new beginning of remaining str
 	printf("test_3: %s\n", temp_2);
 	printf("test_4: %s\n", temp_1);
 	str = temp_2; // sets static str to the ramaining str
-	if (!temp_1)
-		return (NULL);
 	//printf("test_5: %s\n", str);
 	return (temp_1);
 }
