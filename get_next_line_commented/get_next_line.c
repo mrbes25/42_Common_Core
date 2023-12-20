78dbc7d7d9548866eca60447b8e440453268061d
mrbes25 <bastian123schmid@gmail.com>
2023-12-20 17:24:59 +0100

    added part of comments

--- get_next_line_commented/get_next_line.c
+++ get_next_line_commented/get_next_line.c
@@ -18,29 +18,30 @@ static char	*move_it(char **str)
 	char	*newline;
 	char	*placeholder_str;
 
-	if (!*str || !**str)
+	if (!*str || !**str) //check for invalid pointer and empty string
 		return (NULL);
-	newline = ft_strchr(*str, '\n');
-	if (newline)
+	newline = ft_strchr(*str, '\n');//set newline to pointer to \n
+	if (newline)//enter if newline is filled
 	{
-		line = ft_strndup(*str, newline - *str + 1);
-		placeholder_str = *str;
-		*str = ft_strdup(newline + 1);
+		line = ft_strndup(*str, newline - *str + 1);//create a line up to that point
+		placeholder_str = *str;//needs to be copied to placeholder so it can take new value
+		*str = ft_strdup(newline + 1);//new value to str
 		free(placeholder_str);
 	}
 	else
-	{
-		line = ft_strdup(*str);
-		free(*str);
+	{//if no newline is found, duplicate the entire content into line
+		line = ft_strdup(*str);//line is now new dynamically allocated
+		free(*str);//so str can be freed
 		*str = NULL;
 	}
 	return (line);
 }
+//it is importand to allways free the old version after duplicating a string
 
 static char	*ft_read_join(int fd, char *str)
 {
 	char	buffer[BUFFER_SIZE + 1];
-	ssize_t	bytes;
+	ssize_t	bytes; //this datatype is designed for this purpos so it is big enough
 	char	*tmp;
 
 	if (!str)
