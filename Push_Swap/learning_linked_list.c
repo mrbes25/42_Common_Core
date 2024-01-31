134e730ef14c7dc2584bf58c1dadc457f313d61f
mrbes25 <bastian123schmid@gmail.com>
2024-01-31 17:30:38 +0100

    practice

--- Push_Swap/learning_linked_list.c
+++ Push_Swap/learning_linked_list.c
@@ -1,31 +1,174 @@
-#include    <stdio.h>
-#include    <stdlib.h>
+// code from lowlevellearning
+// commented by me to understand linked lists
 
-// do a shopping list
-// if i ask "what do i need to buy" output the list
-// if i say "remove item_name" remove said item
-// is i say "add item_name" add said item
+#include <stdio.h>
+#include <stdlib.h>
 
-// define node struct
+typedef struct { //typedef removes the necessity to tyoe struct every time
+    void *next;
+    int data;
+} Node; // represents a single node in list
 
-struct Node
+Node *head = NULL; //
+
+// add a node to the list
+Node *addNode(int data)
 {
-    char    itemName[100];
-    struct  Node* next;
+    Node *new = NULL;
+    // two cases:
+
+    // if the list is empty.
+    if (head == NULL)
+    {
+        new = malloc(sizeof(Node));
+        if (new == NULL)
+            return NULL;
+        
+        new->data = data;
+        head = new;
+        new->next = NULL;
+    } else {
+        new = malloc(sizeof(Node));
+        if (new == NULL)
+            return NULL;
+        
+        new->data = data;
+        new->next = head;
+        head = new;
+    }
+
+    return new;
 }
 
-struct Node* createNode(int value)
+// remove a node from the list
+int removeNode(int data)
 {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
+    Node *current = head;
+    Node *prev = head;
+    while (current != NULL)
+    {
+        if (current->data == data)
+        {
+            // if current node is the list head
+            if (current == head)
+            {
+                head = current->next;
+            } else {
+                prev->next = current->next;
+                free(current);
+                current = NULL;
+            }
+
+            return 1;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    return 0;
 }
 
-// function to add an item to the list
-void    addItem(struct Node** head, const char* itemName)
+// insert a node into a position in the list
+Node *insertNode(int data, int position)
 {
-	struct Node* newNode = createNode(itemName);
-	newNode->next = *head;
-	*head = newNode;
+    Node *current = head;
+    while (current != NULL && position != 0)
+    {
+        position--;
+    }
+
+    if (position != 0)
+    {
+        printf("Requested position too far into list\n");
+        return NULL;
+    }
+
+    Node *new = malloc(sizeof(Node));
+    if (new == NULL)
+        return NULL;
+
+    new->data = data;
+    new->next = current->next;
+    current->next = new;
+
+    return new;
+}
+
+// print operation
+void printList()
+{
+    Node *current = head;
+    while (current != NULL)
+    {
+        printf("%d->", current->data);
+        current = current->next;
+    }
+
+    printf("\n");
+    return;
+}
+
+void printMenu()
+{
+    printf("You have the following options:\n");
+    printf("\t1. Add a node to the list.\n");
+    printf("\t2. Remove a node from the list.\n");
+    printf("\t3. Insert a node to the list.\n");
+    printf("\t4. Print your list\n");
+    printf("\t5. Quit.\n");
+    return;
+}
+
+
+int main(int argc, char **argv)
+{
+    int option = -1;
+    int arg1 = 0;
+    int arg2 = 0;
+    while (option != 5)
+    {
+        printMenu();
+        int num_received = scanf("%d", &option);
+        if (num_received == 1 && option > 0 && option <= 5)
+        {
+            switch(option)
+            {
+                case 1:
+                    // add operation
+                    printf("What data should I insert?:\n");
+                    scanf("%d", &arg1);
+                    Node *new = addNode(arg1);
+                    break;
+                case 2:
+                    // remove operation
+                    printf("What data should I remove?:\n");
+                    scanf("%d", &arg1);
+                    int success = removeNode(arg1);
+                    if (!success)
+                        printf("Element not found\n");
+
+                    break;
+                case 3:
+                    // insert operation
+                    // remove operation
+                    printf("What data should I insert?:\n");
+                    scanf("%d", &arg1);
+                    printf("What position?:\n");
+                    scanf("%d", &arg2);
+                    new = insertNode(arg1, arg2);
+                    if (new == NULL)
+                        printf("Failed to insert into list\n");
+                    break;
+                case 4:
+                    // print the list
+                    printList();
+                    break;
+                case 5:
+                    break;
+            }
+        }
+    }
+    
+
+    return 0;
 }
